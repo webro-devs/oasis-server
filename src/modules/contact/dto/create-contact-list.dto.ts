29d7d067d1e627e1d343db982be60ef66c778e30
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2024-02-16 19:06:15 +0500

    ...

--- src/modules/contact/dto/create-contact-list.dto.ts
+++ src/modules/contact/dto/create-contact-list.dto.ts
@@ -3,23 +3,35 @@ import { ApiProperty } from '@nestjs/swagger';
 import CreateContactDto from './create-contact.dto';
 
 class CreateContactList {
-    @ApiProperty({
-      description: `title`,
-      isArray:true,
-      example: {
-        title:'',
-        address:'',
-        email:'',
-        phone:'',
-        website:'',
-        workingDays:'',
-        workingHours:'',
-        langCode:''
+  @ApiProperty({
+    description: `title`,
+    isArray: true,
+    example: [
+      {
+        title: '',
+        address: '',
+        email: '',
+        phone: '',
+        website: '',
+        workingDays: '',
+        workingHours: '',
+        langCode: '',
       },
-    })
-    @IsNotEmpty()
-    @IsArray()
-    contents: CreateContactDto[]
-  }
+      {
+        title: '',
+        address: '',
+        email: '',
+        phone: '',
+        website: '',
+        workingDays: '',
+        workingHours: '',
+        langCode: '',
+      },
+    ],
+  })
+  @IsNotEmpty()
+  @IsArray()
+  contents: CreateContactDto[];
+}
 
-  export default CreateContactList
+export default CreateContactList;
